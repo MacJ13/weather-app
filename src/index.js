563324ba348aac968730e446a7538c17996cd217
MacJ13 <jacdim02@gmail.com>
2023-04-06 21:42:07 +0200

    Modify js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 import "./sass/main.scss";
 
 console.log("hello");
+
+const url_weather =
+  "https://api.openweathermap.org/data/2.5/weather?q=jozefow&units=metric&appid=7e81e22c2388addd9806a42c9ea870fd";
+
+// const url_forecast =
+//   "https://api.openweathermap.org/data/2.5/forecast?q=jozefow&appid=7e81e22c2388addd9806a42c9ea870fd";
+
+const getWeather = async function () {
+  //   const resp = await fetch(url_forecast, { mode: "cors" });
+  //   const resp = await fetch(url_weather, { mode: "cors" });
+  //   const weatherData = await resp.json();
+  //   console.log(weatherData);
+};
+
+getWeather();
